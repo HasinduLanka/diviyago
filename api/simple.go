423c785fbff2059a7a00abe52a327be673706721
HasinduLanka <hlrathnayaka@gmail.com>
2022-05-08 20:23:52 +0530

    exp: app images

--- api/simple.go
+++ api/simple.go
@@ -4,26 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/HasinduLanka/diviyago/goex"
 )
 
 func SimpleEndpoint(wr http.ResponseWriter, req *http.Request) {
 
-	libwasmerBytes, libwasmerErr := goex.GetFile("libwasmer.so")
-
-	if libwasmerErr != nil {
-		log.Fatal(libwasmerErr)
-	}
-
-	os.WriteFile("libwasmer.so", libwasmerBytes, 0644)
-
-	os.Mkdir("packaged/lib/linux-amd64/", 0777)
-
-	os.WriteFile("packaged/lib/linux-amd64/libwasmer.so", libwasmerBytes, 0644)
-
-	result, exeErr := goex.ExcecProgramToString("diviya")
+	result, exeErr := goex.ExcecProgramToString("magick")
 
 	if exeErr != nil {
 		log.Panicln("/api/simple : exe error : ", exeErr)
