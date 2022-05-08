2f6d7616b546f05da06cd5268694af5315796a5f
HasinduLanka <hlrathnayaka@gmail.com>
2022-05-08 15:36:13 +0530

    exp: include files on vercel

--- api/hello.go
+++ api/hello.go
@@ -1,9 +1,45 @@
 package api
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func HelloEndpoint(w http.ResponseWriter, r *http.Request) {
+
+	cwd, cwdErr := os.Getwd()
+
+	if cwdErr != nil {
+		log.Panicln("/api/hello : cwd error : ", cwdErr)
+		w.Write([]byte("/api/hello : cwd error : " + cwdErr.Error()))
+		return
+	}
+
+	// list all files in current directory
+
+	files, filesErr := os.Open(cwd)
+
+	if filesErr != nil {
+		log.Panicln("/api/hello : files error : ", filesErr)
+		w.Write([]byte("/api/hello : files error : " + filesErr.Error()))
+		return
+	}
+
+	defer files.Close()
+
+	filesInfo, filesInfoErr := files.Readdir(-1)
+
+	if filesInfoErr != nil {
+		log.Panicln("/api/hello : filesInfo error : ", filesInfoErr)
+		w.Write([]byte("/api/hello : filesInfo error : " + filesInfoErr.Error()))
+		return
+	}
+
+	for _, file := range filesInfo {
+		fmt.Fprintln(w, file.Name()+"\n")
+	}
+
 	w.Write([]byte("HelloEndpoint"))
 }
